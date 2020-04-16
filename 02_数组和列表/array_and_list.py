331d7dc3870c3785d7923498b28f5c69cf2ea4fc
janus1345 <tanyao1345@163.com>
2020-04-16 17:21:14 +0800

    Array ADT

--- 02_数组和列表/array_and_list.py
+++ 02_数组和列表/array_and_list.py
@@ -17,8 +17,30 @@ print(arr[0], arr[1], arr[2], arr[3], type(arr[1]))
 class Array:
 
     def __init__(self, size):
-        self.size = size
+        self._size = size
         self._items = list()
 
     def __getitem__(self, index):
         return self._items[index]
+
+    def __setitem__(self, key, value):
+        self._items[key] = value
+
+    def __len__(self):
+        return self._size
+
+    def clear(self, value=None):
+        for i in range(len(self._items)):
+            self._items[i] = value
+
+    def __iter__(self):
+        for item in self._items:
+            yield item
+
+
+def test_array():
+    size = 10
+    a = Array(size)
+    a[0] = 1
+    assert a[0] == 1
+    assert len(a) == 10
